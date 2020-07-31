22a199d2b2ba9f783d2005081dd2a84ba6afce26
SamsAgency <samstore637@gmail.com>
2020-07-31 19:06:14 +0300

    recipe page modification

--- src/Components/Recipee/Recipe.js
+++ src/Components/Recipee/Recipe.js
@@ -3,7 +3,10 @@ import React from "react";
 const Recipe = (props) => {
     return (
         <div>
-            <h1>{props.title}</h1>
+            <h1>The specific Recipe</h1>
+            <p>This is where we output the single item(recipe) 
+                ingredient and render them here
+            </p>
         </div>
     )
 }
